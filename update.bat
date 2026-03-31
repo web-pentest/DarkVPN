78dbc44c17caaa42e8d2e0bb9f61218561b0d214
TEST <zaika_kisi@mail.ru>
2026-03-31 15:57:47 +0300

    Create update.bat

---
+++ update.bat
@@ -0,0 +1,5 @@
+@echo off
+echo 🔄 DarkVPN — Обновление списка прокси...
+curl -o proxies.txt https://raw.githubusercontent.com/твой-ник/DarkVPN/main/proxies.txt
+echo ✅ Готово! Файл proxies.txt обновлён.
+pause
